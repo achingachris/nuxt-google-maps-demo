e977b236fa5585a83287ccd638607666d6a1eefd
achingachris <achinga.chris@gmail.com>
2023-01-02 21:37:36 +0300

    revised

--- plugins/google-maps.js
+++ plugins/google-maps.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
-import * as VueGoogleMaps from "vue2-google-maps"; // Import package
+import * as VueGoogleMaps from "vue2-google-maps";
+
 Vue.config.productionTip = false;
 Vue.use(VueGoogleMaps, {
   load: {
